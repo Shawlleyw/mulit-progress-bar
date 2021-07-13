e01014b0f74160d8c7625c3b0fae1e6868f764f6
wsy727701 <540253314@qq.com>
2021-07-13 17:29:30 +0800

    add file test.c

--- test.c
+++ test.c
@@ -6,37 +6,93 @@
 
 const int THREAD_N = 3;
 const int BAR_LENGTH = 20;
+typedef enum {
+  PULL,
+  UNTAR,
+  COMPLETE,
+}TASK;
 typedef struct {
-  int progress, count_to_val;
+  size_t total_size;
+  FILE *fp;
+  TASK progress;
   pthread_t tid;
 } thread_info;
 
-void *ThreadTask(void *arg) {
-  thread_info *cur_thread = arg;
-  for (cur_thread->progress = 0;
-      cur_thread->progress < cur_thread->count_to_val;
-      cur_thread->progress++) {
-        usleep(10000); 
+
+void PullTask(thread_info *cur_thread){
+  char *ch = malloc(1024);
+  int chunk_size = 1024;
+  for(int i = 0; i < cur_thread->total_size; i += chunk_size){
+    int lim = chunk_size;
+    if(cur_thread->total_size - i < chunk_size) lim = cur_thread->total_size - i;
+    for(int j = 0; j < lim; j++)
+      ch[j] = 'a' + (i+j) % 26;
+    fwrite(ch, 1, lim, cur_thread->fp);
+    usleep(30);
   }
+  free(ch);
+}
+
+void UntarTask(thread_info *cur_thread){
+  sleep(2);
+}
+
+void *ThreadTask(void *arg) {
+  pthread_detach(pthread_self());
+  thread_info *cur_thread = arg;
+  PullTask(cur_thread);
+  cur_thread->progress = UNTAR;
+  UntarTask(cur_thread);
+  cur_thread->progress = COMPLETE;
   return NULL;
 }
 
-void UpdateBar(thread_info* cur){
-    int percent = (cur->progress * 100 / cur->count_to_val);
-    printf("\r[progressing] [");
-    for (int i = 0; i < BAR_LENGTH; i++){
-        if(i <= percent * BAR_LENGTH / 100)putchar('#');
-        else putchar('-');
-    }
-    printf("] [%d%% / 100%%]\n", percent);
+double CalcSize(int num){
+  return 1.0*num / (1024*1024.0);
 }
+
+void UpdateBar(thread_info* cur){
+  int read_size;
+  if(cur->progress == COMPLETE){
+    read_size = cur->total_size;
+  }
+  else{
+    read_size = ftell(cur->fp);
+  }
+  
+  if(cur->progress == PULL)printf("\rpulling   ");
+  else if(cur->progress == UNTAR)printf("\rextracting");
+  else printf("\rcompleted                   \n ");
+  /**
+  for (int i = 0; i < BAR_LENGTH; i++){
+      if(i <= percent * BAR_LENGTH / 100)putchar('#');
+      else putchar('-');
+  }
+  **/
+  if(cur->progress != COMPLETE)printf("%6.1lfMB/%6.1lfMB\n", CalcSize(read_size), CalcSize(cur->total_size));
+}
+
+FILE *CalcPath(int i){
+  FILE *fp = NULL;
+  char *path = (char*)malloc(30);
+  strcpy(path, "testfile");
+  char ch[2];
+  ch[0] = i + '0';
+  ch[1] = '\0';
+  strcat(path, ch);
+  fp = fopen(path, "wb");
+  return fp;
+}
+
 
 int main(int argc, char **argv) {
   srand(time(NULL));
   thread_info th[THREAD_N];
-  memset(th, -1, sizeof(th));
+  memset(th, 0, sizeof(0));
   for (int i = 0; i < THREAD_N; i++) {
-    th[i].count_to_val = rand() % 1000 + 100;
+    th[i].progress = PULL;
+    th[i].fp = CalcPath(i);
+    th[i].total_size = rand() % 60000000 + 10000000;
     pthread_create(&th[i].tid, NULL, ThreadTask, &th[i]);
   }
   int done = 0;
@@ -44,12 +100,16 @@ int main(int argc, char **argv) {
     done = 1;
     for (int i = 0; i < THREAD_N; i++) {
         UpdateBar(&th[i]);
-        if(th[i].progress != th[i].count_to_val)done = 0;
+        if(th[i].progress != COMPLETE)done = 0;
+        else{
+          free(th[i].fp);
+          th[i].fp = NULL;
+        }
     }
     if(!done){
         printf("\033[%dF", THREAD_N);
     }
-    usleep(10000);
+    usleep(40000);
   }
   return 0;
 }
